22d981536b39ba8a44dc0bddb468d45a17cba93b
Mr. Shah <120000782+Shahupdates@users.noreply.github.com>
2023-06-16 17:26:30 -0400

    Update App.js
    This version of your code makes it much more maintainable and readable. You can easily add or remove links from the navLinks array without having to touch the JSX. The Layout component helps keep the structure consistent across different pages, and lazy loading will improve the load time of your app. I've also included a fallback for when the app is loading components, you could replace <div>Loading...</div> with a custom loading component if you'd like.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,59 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import DocumentConversionPage from './pages/DocumentConversionPage';
-import ChatbotPage from './pages/ChatbotPage';
-import WorkspaceManagementPage from './pages/WorkspaceManagementPage';
-import NotFoundPage from './pages/NotFoundPage';
+
+// Lazy loading the page components
+const HomePage = lazy(() => import('./pages/HomePage'));
+const DocumentConversionPage = lazy(() => import('./pages/DocumentConversionPage'));
+const ChatbotPage = lazy(() => import('./pages/ChatbotPage'));
+const WorkspaceManagementPage = lazy(() => import('./pages/WorkspaceManagementPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+
+const navLinks = [
+  { path: "/", name: "Home" },
+  { path: "/document-conversion", name: "Document Conversion" },
+  { path: "/chatbot", name: "Chatbot" },
+  { path: "/workspace-management", name: "Workspace Management" },
+];
+
+const NavBar = () => (
+  <nav className="navbar">
+    <ul className="nav-links">
+      {navLinks.map(link => (
+        <li key={link.path}>
+          <Link to={link.path}>{link.name}</Link>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
+
+const Layout = ({ children }) => (
+  <div className="container">
+    <header>
+      <h1>DocuFlow</h1>
+    </header>
+    <NavBar />
+    <main>{children}</main>
+    <footer>
+      <p>© 2023 DocuFlow. All rights reserved.</p>
+    </footer>
+  </div>
+);
 
 function App() {
   return (
     <Router>
-      <div>
-        <header>
-          <h1>DocuFlow</h1>
-        </header>
-        <nav className="navbar">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/document-conversion">Document Conversion</Link>
-            </li>
-            <li>
-              <Link to="/chatbot">Chatbot</Link>
-            </li>
-            <li>
-              <Link to="/workspace-management">Workspace Management</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/document-conversion" component={DocumentConversionPage} />
-          <Route path="/chatbot" component={ChatbotPage} />
-          <Route path="/workspace-management" component={WorkspaceManagementPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/document-conversion" component={DocumentConversionPage} />
+            <Route path="/chatbot" component={ChatbotPage} />
+            <Route path="/workspace-management" component={WorkspaceManagementPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </Layout>
+      </Suspense>
     </Router>
   );
 }
